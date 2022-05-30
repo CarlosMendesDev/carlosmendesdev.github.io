db6e01b8c5eeb6bc8663c01ddf4286ab392c9601
Carlos Mendes <52838023+CarlosMendesDev@users.noreply.github.com>
2022-05-30 00:31:55 -0300

    mudanca de portfolio

---
+++ out/_next/static/7HOEVs-RmMyOumSdwr76s/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/413-84b62a8b956d6c70.js","static/css/35f49ae766979e6b.css","static/chunks/pages/index-e85825ed8be62f36.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
